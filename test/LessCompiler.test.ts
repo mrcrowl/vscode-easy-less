e2e8fcbefe7f89407b824b553ad7221c062b588e
Ben Crowl <ben.crowl@hungryhungry.com>
2022-07-16 21:38:11 +1000

    refactor tests

--- test/LessCompiler.test.ts
+++ test/LessCompiler.test.ts
@@ -1,9 +1,9 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { compile } from '../src/LessCompiler';
 import fs from 'fs/promises';
-import less from 'less';
-import { Uri, workspace } from 'vscode';
+import less, { render } from 'less';
+import { afterAll, afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import type { WorkspaceFolder } from 'vscode';
+import { Uri, workspace } from 'vscode';
+import { compile } from '../src/LessCompiler';
 
 vi.mock('fs/promises');
 vi.mock('less');
@@ -16,134 +16,114 @@ vi.mock('vscode', () => {
 
 const CSS_CONTENTS = '.thing.sub { background-color: hotpink }';
 const LESS_CONTENTS = `{ .thing { &.sub { background-color: hotpink; } } }`;
+const MAP_CONTENTS = 'GACxB,IAAMC,GAAUD,EAAGE,SAInB';
 const RENDER_RESULT = { css: CSS_CONTENTS } as Less.RenderOutput;
+const RENDER_RESULT_WITH_MAP = { css: CSS_CONTENTS, map: MAP_CONTENTS } as Less.RenderOutput;
 describe('compile: characterise existing behaviour', () => {
-  beforeEach(() => {
+  const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
+  const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
+
+  afterEach(() => {
     vi.resetAllMocks();
+  });
+
+  afterAll(() => {
+    vi.restoreAllMocks();
   });
 
   it('writes css file - default options', async () => {
     vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-    const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-    const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
 
     const options = {};
     await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-    expect(mkdirSpy).toHaveBeenCalledOnce();
-    expect(mkdirSpy.mock.lastCall).toEqual(['/home/mrcrowl', { recursive: true }]);
-    expect(writeFileSpy).toHaveBeenCalledOnce();
-    expect(writeFileSpy.mock.lastCall).toEqual(['/home/mrcrowl/styles.css', CSS_CONTENTS]);
+    expect(mkdirSpy.mock.calls).toEqual([['/home/mrcrowl', { recursive: true }]]);
+    expect(writeFileSpy.mock.calls).toEqual([['/home/mrcrowl/styles.css', CSS_CONTENTS]]);
   });
 
   describe('out', () => {
-    it('no compile for out: false', async () => {
-      const renderSpy = vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
+    beforeEach(() => {
+      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
+    });
 
+    it('no compile for out: false', async () => {
       const options = { out: false };
       await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-      expect(renderSpy).not.toHaveBeenCalledOnce();
       expect(mkdirSpy).not.toHaveBeenCalledOnce();
       expect(writeFileSpy).not.toHaveBeenCalledOnce();
     });
 
     it('should output to a specific filename (no extension)', async () => {
-      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-
       const options = { out: '/home/mrcrowl/dist/styles' };
-      const result = await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
+      await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-      expect(result).toBeUndefined();
-      expect(mkdirSpy).toHaveBeenCalledOnce();
-      expect(mkdirSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist', { recursive: true }]);
-      expect(writeFileSpy).toHaveBeenCalledOnce();
-      expect(writeFileSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist/styles.css', CSS_CONTENTS]);
+      expect(mkdirSpy.mock.calls).toEqual([['/home/mrcrowl/dist', { recursive: true }]]);
+      expect(writeFileSpy.mock.calls).toEqual([['/home/mrcrowl/dist/styles.css', CSS_CONTENTS]]);
     });
 
     it('should output to a specific filename (with extension)', async () => {
-      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-
       const options = { out: '/home/mrcrowl/dist/styles.css' };
-      const result = await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
+      await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-      expect(result).toBeUndefined();
-      expect(mkdirSpy).toHaveBeenCalledOnce();
-      expect(mkdirSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist', { recursive: true }]);
-      expect(writeFileSpy).toHaveBeenCalledOnce();
-      expect(writeFileSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist/styles.css', CSS_CONTENTS]);
+      expect(mkdirSpy.mock.calls).toEqual([['/home/mrcrowl/dist', { recursive: true }]]);
+      expect(writeFileSpy.mock.calls).toEqual([['/home/mrcrowl/dist/styles.css', CSS_CONTENTS]]);
     });
 
     it('should output to a specific folder (macOS/linux)', async () => {
-      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-
       const options = { out: '/home/mrcrowl/dist/' };
-      const result = await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
+      await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-      expect(result).toBeUndefined();
-      expect(mkdirSpy).toHaveBeenCalledOnce();
-      expect(mkdirSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist', { recursive: true }]);
-      expect(writeFileSpy).toHaveBeenCalledOnce();
-      expect(writeFileSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist/styles.css', CSS_CONTENTS]);
+      expect(mkdirSpy.mock.calls).toEqual([['/home/mrcrowl/dist', { recursive: true }]]);
+      expect(writeFileSpy.mock.calls).toEqual([['/home/mrcrowl/dist/styles.css', CSS_CONTENTS]]);
     });
 
     it('should output with a specific extension (dot prefixed)', async () => {
-      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-
       const options = { out: '/home/mrcrowl/dist/', outExt: '.wxss' };
-      const result = await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
+      await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-      expect(result).toBeUndefined();
-      expect(mkdirSpy).toHaveBeenCalledOnce();
-      expect(mkdirSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist', { recursive: true }]);
-      expect(writeFileSpy).toHaveBeenCalledOnce();
-      expect(writeFileSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist/styles.wxss', CSS_CONTENTS]);
+      expect(mkdirSpy.mock.calls).toEqual([['/home/mrcrowl/dist', { recursive: true }]]);
+      expect(writeFileSpy.mock.calls).toEqual([['/home/mrcrowl/dist/styles.wxss', CSS_CONTENTS]]);
     });
 
     it('should output with a specific extension (no dot)', async () => {
-      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-
       const options = { out: '/home/mrcrowl/dist/', outExt: 'wxss' };
-      const result = await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
+      await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
 
-      expect(result).toBeUndefined();
-      expect(mkdirSpy).toHaveBeenCalledOnce();
-      expect(mkdirSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist', { recursive: true }]);
-      expect(writeFileSpy).toHaveBeenCalledOnce();
-      expect(writeFileSpy.mock.lastCall).toEqual(['/home/mrcrowl/dist/styles.wxss', CSS_CONTENTS]);
+      expect(mkdirSpy.mock.calls).toEqual([['/home/mrcrowl/dist', { recursive: true }]]);
+      expect(writeFileSpy.mock.calls).toEqual([['/home/mrcrowl/dist/styles.wxss', CSS_CONTENTS]]);
     });
 
     it('should interpolate the workspace workspaceFolder', async () => {
-      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT);
-      const mkdirSpy = vi.spyOn(fs, 'mkdir').mockResolvedValue(undefined);
-      const writeFileSpy = vi.spyOn(fs, 'writeFile').mockResolvedValue(undefined);
-
+      // Extra setup for workspace folder path interpolation.
       vi.spyOn(Uri, 'file').mockReturnValue({} as Uri);
-
       const workspaceFolder = { uri: { fsPath: '/home/abc/dev/project' } } as WorkspaceFolder;
       vi.spyOn(workspace, 'getWorkspaceFolder').mockReturnValue(workspaceFolder);
 
       const options = { out: '${workspaceFolder}/test.css' }; // Intentionally NOT a template string here.
       const WORKSPACE_FOLDER = '/home/abc/dev/project';
-      const result = await compile(`${WORKSPACE_FOLDER}/css/test.less`, LESS_CONTENTS, options);
+      await compile(`${WORKSPACE_FOLDER}/css/test.less`, LESS_CONTENTS, options);
 
-      expect(result).toBeUndefined();
-      expect(mkdirSpy).toHaveBeenCalledOnce();
-      expect(mkdirSpy.mock.lastCall).toEqual([`${WORKSPACE_FOLDER}`, { recursive: true }]);
-      expect(writeFileSpy).toHaveBeenCalledOnce();
-      expect(writeFileSpy.mock.lastCall).toEqual([`${WORKSPACE_FOLDER}/test.css`, CSS_CONTENTS]);
+      expect(mkdirSpy.mock.calls).toEqual([[`${WORKSPACE_FOLDER}`, { recursive: true }]]);
+      expect(writeFileSpy.mock.calls).toEqual([[`${WORKSPACE_FOLDER}/test.css`, CSS_CONTENTS]]);
+    });
+  });
+
+  describe('sourceMap', () => {
+    it('should output a sourceMap', async () => {
+      vi.spyOn(less, 'render').mockResolvedValue(RENDER_RESULT_WITH_MAP);
+
+      const options = { sourceMap: true };
+      await compile('/home/mrcrowl/styles.less', LESS_CONTENTS, options);
+
+      expect(mkdirSpy.mock.calls).toEqual([
+        ['/home/mrcrowl', { recursive: true }],
+        ['/home/mrcrowl', { recursive: true }],
+      ]);
+      expect(writeFileSpy.mock.calls).toEqual([
+        ['/home/mrcrowl/styles.css', CSS_CONTENTS],
+        ['/home/mrcrowl/styles.css.map', MAP_CONTENTS],
+      ]);
     });
   });
 });
